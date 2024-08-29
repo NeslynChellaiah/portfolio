2a8aedaaaa8a072d1f799259bdf837b500afb902
Neslyn <neslynchellaiah@gmail.com>
2024-08-28 21:28:53 -0300

    code clean up

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="position-relative main-container">
-      <div className="fixed-top z-1">
-        <Navbar />
-      </div>
+      <Navbar />
       <Me />
       <section className="min-vh-100 bg-secondary p-5" id="works"></section>
       <section className="min-vh-100 bg-primary p-5" id="contact"></section>
