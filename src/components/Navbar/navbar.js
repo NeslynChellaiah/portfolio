117de90fc128a4c58b41482758d848be3a3cbd14
Neslyn <neslynchellaiah@gmail.com>
2024-09-03 21:07:40 -0300

    fixes

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,21 +1,40 @@
 import Nav from "react-bootstrap/Nav";
+import "./navbar.css";
 
 function Navbar() {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="fixed-top z-1">
-      <Nav className="justify-content-center" activeKey="/">
+      <Nav className="justify-content-center" defaultActiveKey="/#me">
         <Nav.Item>
-          <Nav.Link className="text-primary" href="#me">
+          <Nav.Link
+            className="text-primary"
+            onClick={(e) => {
+              scrollToSection("me");
+            }}
+          >
             Me
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link className="text-primary" href="#works">
+          <Nav.Link
+            className="text-primary"
+            onClick={() => scrollToSection("works")}
+          >
             My Works
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link className="text-primary" href="#contact">
+          <Nav.Link
+            className="text-primary"
+            onClick={() => scrollToSection("contact")}
+          >
             Contact Me
           </Nav.Link>
         </Nav.Item>
